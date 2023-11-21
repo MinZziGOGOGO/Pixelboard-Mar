0d03eb30ca5a3c6762316c1175f62b75013e7c6a
MinZziGOGOGO <mzorzi@tsn.at>
2023-11-21 16:29:27 +0100

    Koordinatensystem

--- Code/SPL_4_Koordinatensystem/SPL_4_Koordinatensystem.ino
+++ Code/SPL_4_Koordinatensystem/SPL_4_Koordinatensystem.ino
@@ -10,12 +10,13 @@
 #define PANEL_HEIGHT 16
 
 // Create a CRGB array to store the colors of each LED
-CRGB leds[NUM_LEDS];
+CRGB leds1[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
 
 void setup() {
   // Initialize the FastLED library
-  FastLED.addLeds<WS2812, DATA_PIN1, GRB>(leds, NUM_LEDS);
-  FastLED.addLeds<WS2812, DATA_PIN2, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, DATA_PIN1, GRB>(leds1, NUM_LEDS);
+  FastLED.addLeds<WS2812, DATA_PIN2, GRB>(leds2, NUM_LEDS);
   // Set brightness (0-255)
   FastLED.setBrightness(50);
 
@@ -26,14 +27,15 @@ void setup() {
 void loop() {
   // Call your animation function here
   // In this example, we'll light up a single LED at coordinates (x, y)
-  int x = 2; // X-coordinate
-  int y = 5;  // Y-coordinate
-  setPixel(x, y, CRGB::Red);
+  int x = 22; // X-coordinate
+  int y = 12;  // Y-coordinate
+  setPixel(x, y, CRGB::White);
 
   // Add a delay to slow down the animation
   delay(1000);
   // Clear the LED at the specified coordinates
-  clearPixel(x, y);
+  FastLED.clear();
+  FastLED.show();
   // Add a delay before the next iteration
   delay(1000);
 }
@@ -42,26 +44,29 @@ void loop() {
 void setPixel(int x, int y, CRGB color) {
   // Check if the y-coordinate is even or odd to implement zigzag pattern
   int index;
-  if(y <= PANEL_HEIGHT/2){
-    x=31-x;
-    if (x % 2 == 0) {
-      // Even row
-      index = x * PANEL_HEIGHT/2 + y;
-    } else {
-      // Odd row, reverse direction
-      index = (x + 1) * PANEL_HEIGHT/2 - y - 1;
+    //first Panel
+    if(y<8){
+      x=31-x;
+      if (x % 2 == 0) {
+        // Even row
+        index = x * PANEL_HEIGHT/2 + y;
+      } else {
+        // Odd row, reverse direction
+        index = (x + 1) * PANEL_HEIGHT/2 - y - 1;
+      }
+      leds1[index] = color;
+    }else {
+      y = y - 8;
+      y = 7 - y;
+      if (x % 2 == 0) {
+        // Even row
+        index = x * PANEL_HEIGHT/2 + y;
+      } else {
+        // Odd row, reverse direction
+        index = (x + 1) * PANEL_HEIGHT/2 - y - 1;
+      }
+      leds2[index] = color;
     }
-  }else {
-    y=y-8;
-    if (x % 2 == 0) {
-      // Even row
-      index = x * PANEL_HEIGHT/2 + y + 256;
-    } else {
-      // Odd row, reverse direction
-      index = (x + 1) * PANEL_HEIGHT/2 - y - 1 + 256;
-    }
-  }
-  leds[index] = color;
   FastLED.show();
 }
 
